74afb63af669a9788212caa24fa6e63f80abbda4
Shalaw Fatah <shalaw.fatah@gmail.com>
2021-02-08 12:43:35 -0800

    first commit

---
+++ madlips.py
@@ -0,0 +1,9 @@
+name = input('Write your name: ').capitalize()
+adj = input('Adjective: ').lower()
+verb1 = input('Verb: ').lower()
+verb2 = input('Verb: ').lower()
+famous = input('Famous Person: ').capitalize()
+def madlips():
+    print(f"You are so {adj}, {name}. You like to do {verb1} and {verb2} so much, and yes we all love {famous}, if that's not offensive")
+madlips()
+
